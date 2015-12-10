583a1d539bc048c8e419c7643c19a6438a338d80
Daniel Luna <danieljluna0@gmail.com>
2015-12-09 20:13:23 -0800

    Started Window Interface
    Current goal is to flesh out interfaces for classes before implementing
    anything.

---
+++ Project-Perfect-Citizen/Code/Engine/ppc_computer.h
@@ -0,0 +1,22 @@
+#ifndef PPC_COMPUTER_H
+#define PPC_COMPUTER_H
+
+
+
+
+namespace ppc {
+
+
+///////////////////////////////////////////////////////////////////////
+/// @brief Acts as a fake in-game computer.
+/// @todo Implement Computer class. Flesh out details via Doxygen.
+///////////////////////////////////////////////////////////////////////
+class Computer {
+
+};
+
+
+}       //End namespace ppc
+
+
+#endif  //End PPC_COMPUTER_H
---
+++ Project-Perfect-Citizen/Code/Engine/ppc_window_feature.h
@@ -0,0 +1,22 @@
+#ifndef PPC_WINDOW_FEATURE_H
+#define PPC_WINDOW_FEATURE_H
+
+
+
+
+namespace ppc {
+
+
+///////////////////////////////////////////////////////////////////////
+/// @brief Decorator class for the Window class
+/// @todo Implement WindowFeature Class. Flesh out details via Doxygen.
+///////////////////////////////////////////////////////////////////////
+class WindowFeature {
+
+};
+
+
+}       //End namespace ppc
+
+
+#endif  //End PPC_WINDOW_FEATURE_H
