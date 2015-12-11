a0aecff5ca1bb4b9142001f7f929c21916cc6f9a
Daniel Luna <danieljluna0@gmail.com>
2015-12-10 16:32:57 -0800

    Removed a lot of excess committed files
    Just cleaning up

--- Project-Perfect-Citizen/Code/Engine/ppc_window_feature.h
+++ Project-Perfect-Citizen/Code/Engine/ppc_window_feature.h
@@ -1,7 +1,7 @@
 #ifndef PPC_WINDOW_FEATURE_H
 #define PPC_WINDOW_FEATURE_H
 
-
+#include "ppc_window.h"
 
 
 namespace ppc {
@@ -11,7 +11,7 @@ namespace ppc {
 ///////////////////////////////////////////////////////////////////////
 /// @brief Decorator class for the Window class
 ///////////////////////////////////////////////////////////////////////
-class WindowFeature {
+class WindowFeature : Window {
 
 };
 
