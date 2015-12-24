32f2f89e7baba782a661ba4def8ff4e3987d6cbb
Daniel Luna <danieljluna0@gmail.com>
2015-12-23 17:24:56 -0800

    Updated to VS 2015

--- Project-Perfect-Citizen/Code/Engine/ppc_computer.h
+++ Project-Perfect-Citizen/Code/Engine/ppc_computer.h
@@ -1,8 +1,6 @@
 #ifndef PPC_COMPUTER_H
 #define PPC_COMPUTER_H
 
-#include "ppc_room"
-
 
 namespace ppc {
 
@@ -12,7 +10,7 @@ namespace ppc {
 /// @brief Acts as a fake in-game computer.
 /// @details Maintains the state and representation of a Computer.
 ///////////////////////////////////////////////////////////////////////
-class Computer : Room {
+class Computer {
 
   
 };
