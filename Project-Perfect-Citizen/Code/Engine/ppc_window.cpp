f2001cb7b6827902fc23406676f38fe06ab9d398
Michael Lowe <mimlowe@ucsc.edu>
2016-01-06 07:46:08 -0800

    Drawable Window
    Creating a drawable window class, movable with mouse.

--- Project-Perfect-Citizen/Code/Engine/ppc_window.cpp
+++ Project-Perfect-Citizen/Code/Engine/ppc_window.cpp
@@ -4,3 +4,13 @@
 
 using namespace ppc;
 
+
+
+ppc::Window::Window(float w, float h)
+{
+	
+	sf::Vector2f vec(w,h);
+	setSize(vec);
+	setFillColor(sf::Color(0, 200, 0, 255));
+
+}
--- Project-Perfect-Citizen/Code/Engine/ppc_window.h
+++ Project-Perfect-Citizen/Code/Engine/ppc_window.h
@@ -1,6 +1,7 @@
 #ifndef PPC_WINDOW_H
 #define PPC_WINDOW_H
-
+#include <SFML/Main.hpp>
+#include <SFML/Graphics.hpp>
 #include "ppc_entity.h"
 
 
