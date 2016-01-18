ede7e9596121da5963a2c5be01ae87b285794d96
John Baden <jabaden@mac.com>
2016-01-17 21:49:46 -0800

    Added comments
    boom bam boom

--- Project-Perfect-Citizen/Code/Engine/renderComponent.cpp
+++ Project-Perfect-Citizen/Code/Engine/renderComponent.cpp
@@ -1,6 +1,7 @@
 #include "renderComponent.h"
-
+//You need this when you have a static vector that contains a class data type
 std::vector<RenderComponent*> RenderComponent::renderVector;
+
 
 void RenderComponent::removeElement(int index)
 {
--- Project-Perfect-Citizen/Code/Engine/testRenderSprite.h
+++ Project-Perfect-Citizen/Code/Engine/testRenderSprite.h
@@ -3,17 +3,50 @@
 #include "renderComponent.h"
 #include <string>
 using namespace std;
+/////////////////////////////////////////////////////////////
+/// @brief and example class inheiritng from RenderComponent!
+/// @details I made this class to help anyone in creating a 
+/// renderComponent child Class. Dont be afraid to ask questions
+/// and use this as a template!
+/////////////////////////////////////////////////////////////
 class TestRenderSprite : public RenderComponent {
 private:
-	int privatePlaceholder;
 	sf::Sprite* sprite;
 	sf::Texture* texture;
+	//The index of the sprite inside RenderComponent::renderVector;
 	int vectorIndex;
 public:
+
+////////////////////////////////////////////////////////////////////////////////////
+/// @brief Constructor for TestRenderSprite
+/// @param filname is the String of a file path of a .png file
+////////////////////////////////////////////////////////////////////////////////////
 	TestRenderSprite(string filename);
+
+////////////////////////////////////////////////////////////////////////////////////
+/// @brief Old render function. For now you need to define it to make a renderComponent
+/// as it is a virtual function.
+///////////////////////////////////////////////////////////////////////////////////
 	void render(sf::RenderWindow* wnd);
+
+///////////////////////////////////////////////////////////////////////////////////
+/// @brief Returns a pointer to sprite
+///////////////////////////////////////////////////////////////////////////////////
 	sf::Sprite* getSprite();
+
+///////////////////////////////////////////////////////////////////////////////////
+/// @brief The main draw function that was inheirted from RenderComponent.
+/// @details You need to define this to create a child of RenderComponent
+/// @param target: This is almost always going to be a sf::renderWindow. states:
+/// This is used to dictate certain states that the object can be rendered in. 
+/// For now this can be safely "ignored" Just created a RenderStates object and
+/// shove it in there. 
+////////////////////////////////////////////////////////////////////////////////
 	virtual void draw(sf::RenderTarget & target, sf::RenderStates states) const ;
+//////////////////////////////////////////////////////////////////////////////////
+/// @brief returns the element that the sprite is inside RenderComponent::renderVector
+///////////////////////////////////////////////////////////////////////////////////
 	int getVectorIndex();
+
 	~TestRenderSprite();
 };
