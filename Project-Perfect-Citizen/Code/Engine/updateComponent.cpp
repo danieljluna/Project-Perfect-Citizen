4da138c17cdda58736584d9b180d14fb7c73ff5d
Nader Sleem <nsleem94@gmail.com>
2016-01-17 20:08:53 -0800

    Added filters file to ignore
    Makes live easy

--- Project-Perfect-Citizen/Code/Engine/updateComponent.cpp
+++ Project-Perfect-Citizen/Code/Engine/updateComponent.cpp
@@ -2,6 +2,8 @@
 
 using namespace ppc;
 
+std::vector<updateComponent*> updateComponent::updateVector;
+
 void updateComponent::removeElement(int index) {
 	updateComponent::updateVector.erase(updateComponent::updateVector.begin() + index);
 }
