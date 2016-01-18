a97825810fbbf3eeddf9516706f602a72b6ef899
Daniel Luna <danieljluna0@gmail.com>
2016-01-17 19:36:12 -0800

    Seriously Upgraded Component/Entity
    Woot!

--- Project-Perfect-Citizen/Code/Engine/Entity.h
+++ Project-Perfect-Citizen/Code/Engine/Entity.h
@@ -1,8 +1,8 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
-#include "Component.h"
 
+typedef unsigned int msgType;
 
 namespace ppc {
 
@@ -40,18 +40,77 @@ public:
 
 
   /////////////////////////////////////////////////////////////////////
+  // Getters
+  /////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////
+    /// @brief Returns the number of Components in this Entity.
+    /// 
+    /// @ret Returns the number of Components in this Entity.
+    ///////////////////////////////////////////////////////////////////
+    size_t cmpntCount();
+  
+    ///////////////////////////////////////////////////////////////////
+    /// @brief Returns the Component (or lack thereof) at the specified
+    ///     index in this Entity.
+    /// @pre (0 < index < cmpntCount())
+    ///
+    /// @param index The index to return
+    /// @ret Returns a pointer to the Component with the given index in
+    ///     this Entity.
+    ///////////////////////////////////////////////////////////////////
+    Component* getComponent(size_t index);
+    
+    ///////////////////////////////////////////////////////////////////
+    /// @brief Returns the index of the Component in this Entity.
+    /// 
+    /// @param cmpnt The Component to get the index of.
+    /// @ret Returns the index of cmpnt in this Entity. If cmpnt is not
+    ///     part of this Entity, returns -1.
+    ///////////////////////////////////////////////////////////////////
+    int getIndex(Component* cmpnt);
+
+    
+  /////////////////////////////////////////////////////////////////////
   // Component Manipulation
   /////////////////////////////////////////////////////////////////////
 
-    //TODO: Add functionality to remove a Component
-
     ///////////////////////////////////////////////////////////////////
     /// @brief Adds a Component to the Entity
+    /// @details If the Component is already in the Entity, there will
+    ///     be multiple copies of that entity in the array. Undefined
+    ///     behavior will ensue.
     ///
     /// @param cmpnt Pointer to the Component you wish to add.
-    /// @return The index of the added Component. A return of -1
+    /// @return The index of the added Component. A return of -1 
+    ///     implies the Component was not found.
     ///////////////////////////////////////////////////////////////////
     int addComponent(Component* cmpnt);
+
+    ///////////////////////////////////////////////////////////////////
+    /// @brief Removes the Component at the specified address from the
+    ///     Entity.
+    /// @details If the Component specified is not in the Entity, the
+    ///     function does nothing.
+    /// @post The Component pointed to by cmpnt will not be part of the
+    ///     Entity.
+    ///
+    /// @param cmpnt A pointer to the Entity
+    ///////////////////////////////////////////////////////////////////
+    void removeComponent(Component* cmpnt);
+
+    ///////////////////////////////////////////////////////////////////
+    /// @brief Removes the Component with the specified index from the
+    ///     Entity.
+    /// @details If there is no Component at the given index for this
+    ///     Entity, nothing happens.
+    /// @pre (0 < index < cmpntCount())
+    /// @post getComponent(index) = nullptr
+    /// 
+    /// @param index The index of the desired Component you would like
+    ///     to remove.
+    ///////////////////////////////////////////////////////////////////
+    void removeComponent(size_t index);
     
     
   /////////////////////////////////////////////////////////////////////
@@ -65,7 +124,7 @@ public:
     ///
     /// @param message Denotes the message to send.
     ///////////////////////////////////////////////////////////////////
-    void broadcastMessage(unsigned int message);
+    void broadcastMessage(msgType message);
 
 
 private:
@@ -74,7 +133,10 @@ private:
     static const size_t maxComponentCount = 10;
     
     //Stores a collection of pointers to Components
-    Component* components[maxComponentCount];
+    Component* components_[maxComponentCount];
+
+    //Current Component Count
+    size_t componentCount_;
 
 
 };
