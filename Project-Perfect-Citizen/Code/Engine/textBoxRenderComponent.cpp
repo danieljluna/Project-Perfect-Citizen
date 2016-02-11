066846bd0d09caee37ea312c0d78713bebbe70c6
Michael Lowe <mimlowe@ucsc.edu>
2016-02-11 15:30:06 -0800

    Revert "Text Input Component"

--- Project-Perfect-Citizen/Code/Engine/textBoxRenderComponent.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  textBoxRenderComponent.cpp
-//  Project-Perfect-Citizen
-//
-//  Created by Michael Lowe on 2/11/16.
-//  Copyright © 2016 Hyperfocus Games. All rights reserved.
-//
-
-#include "textBoxRenderComponent.hpp"
