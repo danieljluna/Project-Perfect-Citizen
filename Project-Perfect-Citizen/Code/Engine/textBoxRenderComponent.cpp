ad5d3f172d70d79ca1fd6cee75a026086e8b74de
mimlowe <mimlowe@ucsc.edu>
2016-02-11 15:24:58 -0800

    Forgot to Sync TextBoxRenderComponent

---
+++ Project-Perfect-Citizen/Code/Engine/textBoxRenderComponent.cpp
@@ -0,0 +1,9 @@
+//
+//  textBoxRenderComponent.cpp
+//  Project-Perfect-Citizen
+//
+//  Created by Michael Lowe on 2/11/16.
+//  Copyright © 2016 Hyperfocus Games. All rights reserved.
+//
+
+#include "textBoxRenderComponent.hpp"
