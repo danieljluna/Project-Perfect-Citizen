a8a881183670f90962d70943a590f393a83b86a0
Brandon Gomez <brlgomez@ucsc.edu>
2016-02-15 14:48:42 -0800

    Imported starter code of expressionist Parser

---
+++ Project-Perfect-Citizen/Code/Engine/expressionistParser.cpp
@@ -0,0 +1,9 @@
+//
+//  expressionistParser.cpp
+//  Project-Perfect-Citizen
+//
+//  Created by Brandon Gomez on 2/15/16.
+//  Copyright © 2016 Hyperfocus Games. All rights reserved.
+//
+
+#include "expressionistParser.hpp"
---
+++ Project-Perfect-Citizen/Code/Engine/expressionistParser.hpp
@@ -0,0 +1,85 @@
+//
+//  expressionistParser.hpp
+//  Project-Perfect-Citizen
+//
+//  Created by Brandon Gomez on 2/15/16.
+//  Copyright © 2016 Hyperfocus Games. All rights reserved.
+//
+
+#ifndef expressionistParser_hpp
+#define expressionistParser_hpp
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+namespace ppc {
+    
+class expressionistParser{
+    
+public:
+    
+    ///////////////////////////////////////////////////////////////////
+    ///@brief constructor for parsing expressionist data
+    ///////////////////////////////////////////////////////////////////
+
+    expressionistParser();
+    
+    ///////////////////////////////////////////////////////////////////
+    // Setters
+    ///////////////////////////////////////////////////////////////////
+    void setExpression(std::string expression){
+        expression_ = expression;
+    }
+    void setDeep(bool deep){
+        deep_ = deep;
+    }
+    void setAppRate(int appRate){
+        appRate_ = appRate;
+    }
+    void setExpansion(std::string expansion){
+        expansion_ = expansion;
+    }
+    void setMarkUp(std::vector<std::string> markUp){
+        markUp_ = markUp;
+    }
+    void setRules(std::vector<expressionistParser> rules){
+        rules_ = rules;
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    // Getters
+    ///////////////////////////////////////////////////////////////////
+    
+    std::string getExpression(){
+        return expression_;
+    }
+    bool getDeep(){
+        return deep_;
+    }
+    int getAppRate(){
+        return appRate_;
+    }
+    std::string getExpansion(){
+        return expansion_;
+    }
+    std::vector<std::string> getMarkUp(){
+        return markUp_;
+    }
+    std::vector<expressionistParser> getRules(){
+        return rules_;
+    }
+    
+private:
+    std::string expression_;
+    bool complete_;
+    bool deep_;
+    int appRate_;
+    std::string expansion_;
+    std::vector<std::string> markUp_;
+    std::vector<expressionistParser> rules_;
+};
+    
+};
+
+#endif /* expressionistParser_hpp */
