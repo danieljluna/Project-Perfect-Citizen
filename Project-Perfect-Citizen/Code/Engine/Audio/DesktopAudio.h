a51512631c819c4ecacefe72a73282dc64865767
Andy Baden <jabaden@mac.com>
2016-02-18 18:50:12 -0800

    Audio engine at its core is now implemented.
    Will still be working on it to improve its implementation.
    Comments are finished as well.

--- Project-Perfect-Citizen/Code/Engine/Audio/DesktopAudio.h
+++ Project-Perfect-Citizen/Code/Engine/Audio/DesktopAudio.h
@@ -1,11 +1,57 @@
 #pragma once
 #include "Audio.h"
 #include <iostream>
+#include "SFML/Audio.hpp"
 namespace ppc {
+	//////////////////////////////////////////////////
+	///@brief Pretty much an example child class of
+	///ppc::Audio. However, since we do have desktops,
+	///I envisioned that we could reuse this class 
+	///down the line
+	//////////////////////////////////////////////////
 	class DesktopAudio : public Audio {
+	private:
+		//////////////////////////////////////////////////
+		///@brief sound object that is played by
+		///playSound();
+		///@details in SFML sf::sound acts much the same as 
+		///sf::Sprite. Therefore I named it this way to 
+		///hopefully perpetuate the understanding of 
+		///how the audio engine works
+		//////////////////////////////////////////////////
+		sf::Sound* soundSprite;
+		//////////////////////////////////////////////////
+		///@brief soundbuffer for a test sound
+		///for now, all sounds are frontloaded into memory
+		///and therefore each need a seperate soundBuffer
+		///@details think of soundbuffer as the equivalent
+		///to sf::texture in a texture/sprite pair. A sound
+		///needs to be assigned a "texture" (soundbuffer) to
+		///be played
+		//////////////////////////////////////////////////
+		sf::SoundBuffer* gunshotSound;
+		//////////////////////////////////////////////////
+		///@brief string representation of where sounds are
+		///stored in the IRL file structure of our program
+		///@details setting it up this way allows us to change
+		///less lines of code should the location of the sound
+		///files change
+		//////////////////////////////////////////////////
+		std::string soundDirectory = "Code/Engine/Audio/Sounds/";
 	public:
+		//////////////////////////////////////////////////
+		///@brief Desktop Constructor
+		//////////////////////////////////////////////////
 		DesktopAudio();
+		//////////////////////////////////////////////////
+		///@brief plays a sound. See ppc::Audio::playSound()
+		///for details
+		//////////////////////////////////////////////////
 		virtual void playSound(int sound);
+		//////////////////////////////////////////////////
+		///@brief stops a sound. See ppc::Audio::stopSound()
+		///for details
+		//////////////////////////////////////////////////
 		virtual void stopSound(int sound);
 	};
 };
--- Project-Perfect-Citizen/Code/Engine/Audio/Sounds.h
+++ Project-Perfect-Citizen/Code/Engine/Audio/Sounds.h
@@ -1,5 +1,12 @@
 #pragma once
 namespace ppc {
+	/////////////////////////////////////
+	///@brief enum of Sounds. Use these
+	///when choosing sounds to play.
+	///assuming its implemented,
+	///everything else is done under the 
+	///hood
+	/////////////////////////////////////
 	enum Sounds {
 		click,
 		returnSound,
