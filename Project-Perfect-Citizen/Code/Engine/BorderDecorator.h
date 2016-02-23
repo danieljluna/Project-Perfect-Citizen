7a8949230886500dc5f1dfe40c8601c20b6ed931
Daniel Luna <danieljluna0@gmail.com>
2016-02-23 01:32:10 -0800

    Comments

--- Project-Perfect-Citizen/Code/Engine/BorderDecorator.h
+++ Project-Perfect-Citizen/Code/Engine/BorderDecorator.h
@@ -14,12 +14,30 @@ public:
   // Constructors & Destructor
   /////////////////////////////////////////////////////////////////////
 
+    //No default Constructor
     BorderDecorator() = delete;
 
+    ///////////////////////////////////////////////////////////////////
+    /// @brief Constructor
+    /// @details Defines what Window the BorderDecorator is decorating.
+    ///     The constructor's optional arguments define the width of 
+    ///     the border around the decorated Window.
+    ///
+    /// @param win A reference to the WindowInterface to be decorated
+    ///     by this BorderDecorator.
+    /// @param majorBorder Defines the width of the border at the top
+    ///     of the Window where the caption and buttons will be drawn.
+    /// @param minorBorder Defines the width of the border around the
+    ///     rest of the Window, and is simply used to outline the 
+    ///     Window.
+    ///////////////////////////////////////////////////////////////////
     BorderDecorator(WindowInterface& win, 
                     unsigned int majorBorder = 34, 
                     unsigned int minorBorder = 5);
 
+    ///////////////////////////////////////////////////////////////////
+    /// @brief Virtual Destructor.
+    ///////////////////////////////////////////////////////////////////
     virtual ~BorderDecorator();
 
 
@@ -48,15 +66,24 @@ private:
 
     void updateBounds();
 
+
   /////////////////////////////////////////////////////////////////////
   // Private Variables 
   /////////////////////////////////////////////////////////////////////
 
+    //Stores the vector from the top left of the Window to the top left
+    //  of the border.
     sf::Vector2u borderTopLeft_;
+
+    //Stores the vector from the bottom right of the Window to the
+    //  bottom right of the border.
     sf::Vector2u borderBottomRight_;
 
+    //Stores the rectangle that is used to display the border.
     sf::RectangleShape borderShape_;
 
+    //Stores the input Component that allows the Window to be dragged 
+    //  via the BorderDecorator.
     DraggableInput draggableInput_;
 
 
