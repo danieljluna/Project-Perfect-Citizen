c6279ed66b09426dbe32cbb748850bb8c9886304
Jabaden <jabaden@mac.com>
2016-04-19 16:55:43 -0700

    little red dot in corner
    ayy lmao

--- Project-Perfect-Citizen/Code/Game/buttonRenderComponent.cpp
+++ Project-Perfect-Citizen/Code/Game/buttonRenderComponent.cpp
@@ -28,16 +28,16 @@ buttonRenderComponent::buttonRenderComponent( sf::Image& image,
     yIndex = y;
 
     //Set up circle shape
-    badge_.setFillColor(sf::Color::Red);
-    badge_.setOutlineColor(sf::Color::White);
-    badge_.setOutlineThickness(2.f);
-    badge_.setRadius(5.f);
+    //badge_.setFillColor(sf::Color::Red);
+    //badge_.setOutlineColor(sf::Color::White);
+    //badge_.setOutlineThickness(2.f);
+    //badge_.setRadius(5.f);
 
     //set up text
-    notificationText_ = sf::Text(std::to_string(5),
-                                 World::getFont(World::Consola),
-                                 10);
-    notificationText_.move(2.5f, -2.0f);
+    //notificationText_ = sf::Text(std::to_string(5),
+      //                           World::getFont(World::Consola),
+        //                         10);
+    //notificationText_.move(2.5f, -2.0f);
     //notificationText_.setStyle(sf::Text::Bold);
     
 
@@ -119,11 +119,11 @@ void buttonRenderComponent::draw( sf::RenderTarget& target,
         target.draw(*sprite, states);
         if (getButtonType() == "ICON") {
 
-            states.transform.translate(sprite->getPosition());
-            states.transform.scale(1.3, 1.3, 0, 0);
+            //states.transform.translate(sprite->getPosition());
+            //states.transform.scale(1.3, 1.3, 0, 0);
 
-            target.draw(badge_, states);
-            target.draw(notificationText_, states);
+            //target.draw(badge_, states);
+            //target.draw(notificationText_, states);
         }
 }
 
--- Project-Perfect-Citizen/Code/Game/notifcationRenderComponent.h
+++ Project-Perfect-Citizen/Code/Game/notifcationRenderComponent.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "../Engine/renderComponent.h"
+
+namespace ppc {
+
+	class notificationRenderComponent : public ppc::RenderComponent {
+	private:
+		sf::CircleShape badge_;
+		sf::Text notificationText_;
+	public:
+		notificationRenderComponent();
+		virtual void draw(sf::RenderTarget & target, sf::RenderStates states) const;
+
+	};
+};
