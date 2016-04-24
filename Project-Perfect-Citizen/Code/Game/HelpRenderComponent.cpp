39e10c149701fcdee7de2eadf9bafdeeffc14419
mimlowe <mimlowe@ucsc.edu>
2016-04-23 18:13:56 -0700

    Floppy Render Updates
    Floppy cycles through all his emotions

--- Project-Perfect-Citizen/Code/Game/HelpRenderComponent.cpp
+++ Project-Perfect-Citizen/Code/Game/HelpRenderComponent.cpp
@@ -29,19 +29,18 @@ HelpWindowRenderComponent::HelpWindowRenderComponent(sf::Font& f, int x, int y, 
 
     consoleHelpString = "--------------------- CONSOLE REFERENCE --------------------\n";
     consoleHelpString += "\n\n Commands \n\n pwd : prints the current working directory";
+    consoleHelpString += "\n\n pwd : Prints the current working directory";
     consoleHelpString += "\n\n cd [path] : changes the current directory";
     consoleHelpString += "\n\n ls : lists the contents of the current directory";
-    consoleHelpString += "\n\n make [filename][contents] : creates a new text file";
-    consoleHelpString += "\n\n mkdir [name] : creates a new directory";
+    consoleHelpString += "\n\n unlock [path] [password] : Unlocks the directory at the \n specified path";
+    consoleHelpString += "\n\n help : Displays a list of possible console commands";
+    
 
 
     graphHelpString = "--------------------- GRAPH REFERENCE --------------------\n";
     graphHelpString += "Node Selection: Left Click on a node, node will be highlighted.\n (Edges will be deselected)\n Edge Selection: Left click on an edge (Nodes will be deselected)\n\n  Node Position: Click and drag a node to reposition it.\n Edges are updated automatically.\n Node position is to help you work towards a solution,\n and does not affect correctness.\n\n Change Color of Selected Edge: \n Z - Black,\n X - Red,\n  C - Green,\n Space - Cycle Color.  \n  \n Edge Color Significance:\n Black - Unknown,\n Red - Suspicious,\n Green - Clean.\n\n Create an Edge: Select a node, then right click the node\n you want to connect it to.\n \n  Delete an Edge:\n Select the edge, press DEL Mark Center of Graph\n - S Submit for Approval\n - Okay Button For this first trial you will simply be searching\nfor the person with the highest total number of connections.\n You must still fill out the rest of the network\nwith high accuracy to receive approval.";
  
-    
 
-    
-    
     contentText->setString(consoleHelpString);
     
    /* helpContentBox.setPosition(sf::Vector2f(0.0f, 0.0f));
