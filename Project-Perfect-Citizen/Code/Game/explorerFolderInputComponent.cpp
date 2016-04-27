8df61abfcb99c504bc8763ca3c20d45ec6fa05d5
Alex Vincent <alexraymondvincent@gmail.com>
2016-04-27 11:56:39 -0700

    - fixed animation explorer folder (may change this due to playtest feedback)

--- Project-Perfect-Citizen/Code/Game/explorerFolderInputComponent.cpp
+++ Project-Perfect-Citizen/Code/Game/explorerFolderInputComponent.cpp
@@ -81,7 +81,11 @@ bool explorerFolderInputComponent::registerInput(Event ppcEv) {
 				isCollision({ ev.mouseButton.x ,ev.mouseButton.y })) {
 
 				/* Send the mouse down message regardless */
-				getEntity()->broadcastMessage(MOUSE_DOWN_CODE);
+				ppc::Event clickEvent;
+				clickEvent.type = Event::EventTypes::ButtonType;
+				clickEvent.buttons.state = Event::ButtonsEv::Clicked;
+				clickEvent.buttons.activation = Event::ButtonsEv::LeftMouse;
+				getEntity()->broadcastMessage(clickEvent);
 				onPress_.sendEvent(ev);
 
 				/* Handle Double Click Register */
@@ -138,6 +142,13 @@ bool explorerFolderInputComponent::registerInput(Event ppcEv) {
 			spawnContextMenu(theDesktop_, ContextMenu, ContextMenu->getInputHandler(), elementNames,
 				elementFunctions, ev.mouseButton.x+containingWindow_->getPosition().x, ev.mouseButton.y + containingWindow_->getPosition().y);
 			theDesktop_.addWindow(ContextMenu);
+			}
+		else if (ev.mouseButton.button == sf::Mouse::Left) {
+			ppc::Event clickEvent;
+			clickEvent.type = Event::EventTypes::ButtonType;
+			clickEvent.buttons.state = Event::ButtonsEv::Release;
+			clickEvent.buttons.activation = Event::ButtonsEv::LeftMouse;
+			getEntity()->broadcastMessage(clickEvent);
 			}
 		}
 	}
