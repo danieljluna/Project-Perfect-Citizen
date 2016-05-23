1f4e65ff6d6f4a714d735a52799492d6427d759f
mimlowe <mimlowe@ucsc.edu>
2016-05-22 18:25:54 -0700

    Fixed button render
    scroll bar doesn’t crash

--- Project-Perfect-Citizen/Code/main.cpp
+++ Project-Perfect-Citizen/Code/main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char** argv) {
 		AudioQueue audiotest(5);
 		audiotest.addBgm("SoundTrack_Extraction.ogg");
 		audiotest.loopBgm();
-		audiotest.playBgm();
+		//audiotest.playBgm();
 
 		///////////////////////////////////////////////////////////////////
 
