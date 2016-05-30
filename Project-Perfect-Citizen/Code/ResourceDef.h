40fed4e1b6ebd4ab76661ab39e1cb4d89f804dc9
Daniel Luna <danieljluna0@gmail.com>
2016-05-30 15:58:26 -0700

    Windows installer is functional

--- Project-Perfect-Citizen/Code/ResourceDef.h
+++ Project-Perfect-Citizen/Code/ResourceDef.h
@@ -3,7 +3,7 @@
 //Used to get XCODE working/////////////////////////////////
 
 #ifdef WINDOWS_MARKER
-#define resourcePath() std::string("Resources/")
+#define resourcePath() std::string("../Resources/")
 #else
 #include "ResourcePath.hpp"
 #endif
