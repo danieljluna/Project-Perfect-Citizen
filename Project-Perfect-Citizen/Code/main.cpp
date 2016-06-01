078d7324d8802ba725461f5fee206f2e58f6b881
Brandon Gomez <brlgomez@ucsc.edu>
2016-06-01 14:21:27 -0700

    Deleted thing on main

--- Project-Perfect-Citizen/Code/main.cpp
+++ Project-Perfect-Citizen/Code/main.cpp
@@ -66,8 +66,6 @@ int main(int argc, char** argv) {
 		World::loadState("PPC.sav");
 		std::ifstream desktopFileInput;
 
-        World::setCurrDesktopEnum(World::DE3);
-
 		while (World::getCurrDesktopEnum() != World::DesktopCount) {
 			Desktop mainDesktop;
 			World::setCurrDesktop(mainDesktop);
