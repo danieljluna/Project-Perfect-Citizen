12a994eca1ec06079a208bb18adff56696915ef4
Brandon Gomez <brlgomez@ucsc.edu>
2016-06-01 19:46:20 -0700

    push

--- Project-Perfect-Citizen/Code/main.cpp
+++ Project-Perfect-Citizen/Code/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char** argv) {
 
 		World::loadState("PPC.sav");
 		std::ifstream desktopFileInput;
-        World::setCurrDesktopEnum(World::DE2B);
+        //World::setCurrDesktopEnum(World::DE2B);
 		while (World::getCurrDesktopEnum() != World::DesktopCount) {
 			Desktop mainDesktop;
 			World::setCurrDesktop(mainDesktop);
