c38fa07ef9e467e5ae82fbfcffd08df78fa30a66
Alex Vincent <alexraymondvincent@gmail.com>
2016-06-02 15:55:54 -0700

    - other hacker desktop changes

--- Project-Perfect-Citizen/Code/main.cpp
+++ Project-Perfect-Citizen/Code/main.cpp
@@ -58,7 +58,6 @@ int main(int argc, char** argv) {
 		World::loadState("PPC.sav");
 		std::ifstream desktopFileInput;
 
-		World::setCurrDesktopEnum(World::DE3);
 		while (World::getCurrDesktopEnum() != World::DesktopCount) {
 			Desktop mainDesktop;
 			World::setCurrDesktop(mainDesktop);
